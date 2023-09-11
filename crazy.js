35deca73435fc6848fdf6b291ae26fa5e2d4564e
TopDev14 <pocketdev2022@gmail.com>
2023-09-11 00:18:35 -0700

    Update crazy.js

--- crazy.js
+++ crazy.js
@@ -5,10 +5,6 @@ function crazy() {
   const bruh = 'bruh';
   const nahbruh = 'bruh';
   const haha = 'haha'
-  // Honnkvjhbknsdafasgd
-  //sjeoafijwoiejsdfbsfaf
-  //asdfjalskjdffhiasudhfpoiashefpoih
-  // ajsodkjf;aosdysgwdgfafiusdhfiu
-  ///dfajsdf;ajsdf;lkjads;flfdredgadads
-  ///sdfjyhfgnflkn thisishasoidfhoasioihwegiuashposdidhf
-}
+  // Honnkvsgddsf
+  //sjeoafijwoijsdfbsfj
+  //
